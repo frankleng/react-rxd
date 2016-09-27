f2124042b2e83d0cbc72d41b9881c66c6d94d9d8
David Beckley <david@realstax.com>
2016-09-26 17:14:49 -0700

    Add checkbox component

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     Background: './src/Background/index.js',
     Button: './src/Button/index.js',
     Card: './src/Card/index.js',
+    Checkbox: './src/Checkbox/index.js',
     Grid: './src/Grid/index.js',
     Input: './src/Input/index.js',
     MediaObject: './src/MediaObject/index.js',
@@ -74,14 +75,6 @@ module.exports = {
           'style',
           'css?module&localIdentName=[name]_[local]!sass'
         )
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000'
       }
     ]
   }
