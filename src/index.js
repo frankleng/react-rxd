39efe37bbff5f4ba7a4203b3ac7629086bb986b6
David Beckley <beckl.ds@gmail.com>
2016-09-26 17:46:58 -0700

    Add checkbox component (#8)
    * Add checkbox component
    
    * Slightly cleaner

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export Avatar from './Avatar';
 export Background from './Background';
 export Button from './Button';
 export Card from './Card';
+export Checkbox from './Checkbox';
 export Grid from './Grid';
 export Input from './Input';
 export Table from './Table';
