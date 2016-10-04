f26650497dc3eecda819825ab05b9469c823c607
Frank Leng <frank@realstax.com>
2016-10-04 16:03:42 -0700

    remove font loaders

--- webpack.config.js
+++ webpack.config.js
@@ -77,14 +77,6 @@ module.exports = {
           'style',
           'css?module&localIdentName=[name]_[local]!sass'
         )
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000'
       }
     ]
   }
