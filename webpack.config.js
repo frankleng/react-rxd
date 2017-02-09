09bebf3eb2c95975de6a0db74da64039fa6f0f0d
Frank Leng <frank@realstax.com>
2017-02-09 03:32:22 -0500

    better build pipeline. fixes BWEB-490

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ExternalsPlugin = require('webpack-externals-plugin');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'source-map',
 
+  externals : {
+    react: 'react',
+    lodash: 'lodash'
+  },
+
   entry: {
+    'postcss.config': './postcss.config.js',
     AjaxLoader: './src/AjaxLoader/index.js',
     Animatable: './src/Animatable/index.js',
     Avatar: './src/Avatar/index.js',
@@ -38,49 +43,42 @@ module.exports = {
   },
 
   resolve: {
-    modulesDirectories: [
+    modules: [
       'src',
       'node_modules'
     ],
     alias: { 'react/lib/ReactUpdates': 'react-dom/lib/ReactUpdates' },
-    extensions: ['', '.json', '.js']
+    extensions: ['.json', '.js']
   },
 
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new ExternalsPlugin({
-      type: 'commonjs',
-      include: path.join(__dirname, '../node_modules/')
-    }),
-    new ExtractTextPlugin('rxd', '[name].css', { allChunks: true }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
+    new ExtractTextPlugin({ filename: '[name].css', allChunks: true })
   ],
 
   module: {
     loaders: [
       {
         test: /\.(jpe?g|png|gif|svg)$/,
-        loader: 'url'
+        loader: 'url-loader'
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: 'json-loader'
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css?module&localIdentName=[name]_[local]!sass'
-        )
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?minimize&-zindex&module&localIdentName=[name]_[local]!sass-loader'
+        })
       }
     ]
   }
