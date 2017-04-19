b0eb484616cf1349afae06238cd38c73d0d82826
Frank Leng <frank@realstax.com>
2017-04-18 20:16:49 -0700

    fix controlled Input bug, fixed modal backdrop.

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,33 +1,36 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
 const classnames = require('classnames');
-
 const styles = require('./Modal.scss');
+const _ = require('lodash');
 
 const stopPropagation = (e) => e.stopPropagation();
 
 class Modal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      show: false
-    };
+    this.appRootId = props.appRootId || 'app';
+    this.modalPortalId = props.modalPortalId || 'afterApp';
+    this.wrapperNode = null;
+    this.modalId = null;
   }
 
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener('DOMContentLoaded', this.init);
+    this.init();
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.show !== this.state.show) {
+    if (nextProps.show !== this.props.show) {
       if (nextProps.show) {
-        document.body.classList.add(styles.disableScroll);
-        this.setState({ show: nextProps.show });
+        this.wrapperNode.classList.add(styles.modalOpened);
+        this.renderIntoDOM(nextProps);
       } else {
-        this.setState({ hiding: true });
-
+        this.renderIntoDOM(nextProps);
         window.setTimeout(() => {
-          document.body.classList.remove(styles.disableScroll);
-          this.setState({ hiding: false, show: false });
+          this.wrapperNode.classList.remove(styles.modalOpened);
+          document.getElementById(this.modalId).innerHTML = '';
         }, 0.2 * 1000);
       }
     }
@@ -35,27 +38,48 @@ class Modal extends React.Component {
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
-    document.body.classList.remove(styles.disableScroll);
+    document.removeEventListener('DOMContentLoaded', this.init);
+    if (this.wrapperNode) this.wrapperNode.classList.remove(styles.modalOpened);
+    if (this.modalPortalNode) this.modalPortalNode.removeChild(document.getElementById(this.modalId));
+    delete this.wrapperNode;
+    delete this.modalNode;
+    delete this.modalPortalNode;
   }
+
+  init = () => {
+    this.modalId = _.uniqueId('modal_');
+    this.wrapperNode = document.getElementById(this.appRootId);
+    this.modalPortalNode = document.getElementById(this.modalPortalId);
+    if (this.modalPortalNode) {
+      this.modalNode = document.createElement('div');
+      this.modalNode.id = this.modalId;
+      this.modalPortalNode.appendChild(this.modalNode);
+    }
+  };
 
   handleKeyDown = e => {
     if (e.keyCode === 27)
       this.props.onCancel();
   };
 
-  render() {
-    const { onCancel, children, small, medium, large, noBackdrop } = this.props;
-    return (
+  renderIntoDOM = (props) => {
+    const { onCancel, children, small, medium, large, noBackdrop, show } = props;
+    ReactDOM.render(
       <div
-        className={classnames(styles.container, { [styles.show]: this.state.show, [styles.hiding]: this.state.hiding })}
+        className={classnames(styles.container, { [styles.show]: show, [styles.hiding]: !show })}
         onClick={onCancel}
       >
         {!noBackdrop && <div className={styles.modalBackdrop} />}
         <div className={classnames(styles.modal, { [styles.small]: small, [styles.medium]: medium, [styles.large]: large })} onClick={stopPropagation}>
           {children}
         </div>
-      </div>
+      </div>,
+      document.getElementById(this.modalId)
     );
+  };
+
+  render() {
+    return null;
   }
 }
 
@@ -65,7 +89,10 @@ Modal.propTypes = {
   small: React.PropTypes.bool,
   medium: React.PropTypes.bool,
   large: React.PropTypes.bool,
-  noBackdrop: React.PropTypes.bool
+  noBackdrop: React.PropTypes.bool,
+  appRootId: React.PropTypes.string,
+  modalPortalId: React.PropTypes.string,
+  show: React.PropTypes.bool
 };
 
 export const Header = ({ children, onCancel }) => (
